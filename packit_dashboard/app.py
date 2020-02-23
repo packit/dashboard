24fb78bf567ddbc1ef72a1725a19ff60b619336b
Anchit <ab@abifog.com>
2020-02-23 23:55:42 +0530

    Add copr builds tab

--- packit_dashboard/app.py
+++ packit_dashboard/app.py
@@ -98,6 +98,19 @@ def projects():
     )
     return render_template("main_frame.html", header="Project", content=content)
 
+@app.route("/builds/")
+def builds():
+    # Only display recent builds for now
+    # Fetching everything takes too long
+    # TODO Add button/table navigation to load more
+    json_url = f"{API_URL}/copr-builds?page=1&per_page=20"
+    api_data = requests.get(url=json_url).json()
+    content = render_template(
+        "builds.html",
+        builds_list = api_data
+    )
+    return render_template("main_frame.html", header="Builds", content=content)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
