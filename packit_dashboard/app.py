9bee8da4e5d1a7ac5d4dccb69b67d82cc9bf7229
Anchit <ab@abifog.com>
2020-06-05 16:06:38 +0530

    Configure Flask to serve React based frontend

--- packit_dashboard/app.py
+++ packit_dashboard/app.py
@@ -1,21 +1,13 @@
-from flask import Flask, send_from_directory
+from flask import Flask
 from packit_dashboard.home.routes import home
-from packit_dashboard.about.routes import about
-from packit_dashboard.builds.routes import builds
-from packit_dashboard.projects.routes import projects
+
 
 app = Flask("Packit Service Dashboard")
 
 
-@app.route("/node_modules/<path:filename>")
-def node_modules(filename):
-    return send_from_directory(f"node_modules", filename)
-
-
+# Note: Declare any other flask blueprints or routes above this
+# Routes declared below this will be rendered by React
 app.register_blueprint(home)
-app.register_blueprint(about)
-app.register_blueprint(builds)
-app.register_blueprint(projects)
 
 
 if __name__ == "__main__":
