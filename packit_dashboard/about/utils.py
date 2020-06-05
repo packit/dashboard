198a58f945255f5e47a8f1f0f18d07b056a62cc2
Anchit <ab@abifog.com>
2020-06-05 16:04:20 +0530

    Remove Flask based frontend

--- packit_dashboard/about/utils.py
+++
@@ -1,8 +0,0 @@
-import socket
-
-
-def check_service(server, port, ip_type=socket.SOCK_STREAM):
-    sock = socket.socket(socket.AF_INET, ip_type)
-    result = sock.connect_ex((server, port))
-    sock.close()
-    return result == 0
--- packit_dashboard/utils.py
+++ packit_dashboard/utils.py
@@ -1,4 +1,17 @@
-from packit_dashboard.packagewide_utils import return_json
+import requests
+import json
+
+
+# Common utility functions used in multiple files in the packit_dashboard package
+# Returns python parsable json object from URL
+def return_json(url, method="GET", **kwargs):
+    response = requests.request(method=method, url=url, **kwargs)
+    output = None
+    try:
+        output = json.loads(response.content)
+    except Exception:
+        pass
+    return output
 
 
 # Takes a base url, and gets json files from multiple pages from that url
