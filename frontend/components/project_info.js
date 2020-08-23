ee4002da32f1e6bfe148e49db9cbfb5927b52a43
Anchit <ab@abifog.com>
2020-08-23 23:27:32 +0530

    Warn if project does not exist in Packit db

--- frontend/components/project_info.js
+++ frontend/components/project_info.js
@@ -1,13 +1,15 @@
-import * as React from "react";
+import React, { useState, useEffect } from "react";
 import {
     PageSection,
     PageSectionVariants,
     Text,
     Tabs,
     Tab,
+    Title,
     TabTitleText,
     Card,
     CardBody,
+    Divider,
     TextContent,
     Label,
 } from "@patternfly/react-core";
@@ -16,8 +18,9 @@ import PullRequestList from "./pr";
 import BranchList from "./branch";
 import IssuesList from "./issues";
 import ReleasesList from "./releases";
-
-import { GithubIcon } from "@patternfly/react-icons";
+import ConnectionError from "./error";
+import Preloader from "./preloader";
+import { GithubIcon, ExternalLinkAltIcon } from "@patternfly/react-icons";
 
 const ProjectInfo = (props) => {
     let forge = props.match.params.forge;
@@ -25,9 +28,103 @@ const ProjectInfo = (props) => {
     let repoName = props.match.params.repoName;
 
     const [activeTabKey, setActiveTabKey] = React.useState(0);
+    const [hasError, setErrors] = useState(false);
+    const [loaded, setLoaded] = useState(false);
+    const [isValidProject, setValidProject] = React.useState(false);
+    const [projectURL, setProjectURL] = React.useState("");
     const handleTabClick = (event, tabIndex) => {
         setActiveTabKey(tabIndex);
     };
+
+    function checkValidProject() {
+        fetch(`${apiURL}/projects/${forge}/${namespace}/${repoName}`)
+            .then((response) => response.json())
+            .then((data) => {
+                if (!data.error) {
+                    setValidProject(true);
+                    setProjectURL(data.project_url);
+                }
+                setLoaded(true);
+            })
+            .catch((err) => {
+                console.log(err);
+                setErrors(err);
+            });
+    }
+
+    // Executes fetchData on first render of component
+    useEffect(() => {
+        checkValidProject();
+    }, []);
+
+    let details = <div>nopeses</div>;
+
+    if (isValidProject && loaded) {
+        details = (
+            <div>
+                <Tabs
+                    isFilled
+                    activeKey={activeTabKey}
+                    onSelect={handleTabClick}
+                    isBox={true}
+                >
+                    <Tab
+                        eventKey={0}
+                        title={<TabTitleText>PRs Handled</TabTitleText>}
+                    >
+                        <PullRequestList
+                            repoName={repoName}
+                            namespace={namespace}
+                            forge={forge}
+                        />
+                    </Tab>
+                    <Tab
+                        eventKey={1}
+                        title={<TabTitleText>Releases Handled</TabTitleText>}
+                    >
+                        <ReleasesList
+                            repoName={repoName}
+                            namespace={namespace}
+                            forge={forge}
+                        />
+                    </Tab>
+                    <Tab
+                        eventKey={2}
+                        title={<TabTitleText>Branches Handled</TabTitleText>}
+                    >
+                        <BranchList
+                            repoName={repoName}
+                            namespace={namespace}
+                            forge={forge}
+                        />
+                    </Tab>
+                    <Tab
+                        eventKey={3}
+                        title={<TabTitleText>Issues Handled</TabTitleText>}
+                    >
+                        <IssuesList
+                            repoName={repoName}
+                            namespace={namespace}
+                            forge={forge}
+                        />
+                    </Tab>
+                </Tabs>
+            </div>
+        );
+    } else if (!isValidProject && loaded) {
+        details = (
+            <Title headingLevel="h1" size="lg">
+                Not Found.
+            </Title>
+        );
+    } else if (!loaded) {
+        details = <Preloader />;
+    }
+
+    // If backend API is down
+    if (hasError) {
+        return <ConnectionError />;
+    }
 
     return (
         <div>
@@ -38,74 +135,27 @@ const ProjectInfo = (props) => {
                         <Label color="blue" icon={<GithubIcon />}>
                             {forge}
                         </Label>
+                        <span style={{ marginLeft: "10px" }}>
+                            <ProjectLink link={projectURL} />
+                        </span>
                     </Text>
                 </TextContent>
             </PageSection>
-
             <PageSection>
                 <Card>
-                    <CardBody>
-                        <Tabs
-                            isFilled
-                            activeKey={activeTabKey}
-                            onSelect={handleTabClick}
-                            isBox={true}
-                        >
-                            <Tab
-                                eventKey={0}
-                                title={<TabTitleText>PRs Handled</TabTitleText>}
-                            >
-                                <PullRequestList
-                                    repoName={repoName}
-                                    namespace={namespace}
-                                    forge={forge}
-                                />
-                            </Tab>
-                            <Tab
-                                eventKey={1}
-                                title={
-                                    <TabTitleText>
-                                        Releases Handled
-                                    </TabTitleText>
-                                }
-                            >
-                                <ReleasesList
-                                    repoName={repoName}
-                                    namespace={namespace}
-                                    forge={forge}
-                                />
-                            </Tab>
-                            <Tab
-                                eventKey={2}
-                                title={
-                                    <TabTitleText>
-                                        Branches Handled
-                                    </TabTitleText>
-                                }
-                            >
-                                <BranchList
-                                    repoName={repoName}
-                                    namespace={namespace}
-                                    forge={forge}
-                                />
-                            </Tab>
-                            <Tab
-                                eventKey={3}
-                                title={
-                                    <TabTitleText>Issues Handled</TabTitleText>
-                                }
-                            >
-                                <IssuesList
-                                    repoName={repoName}
-                                    namespace={namespace}
-                                    forge={forge}
-                                />
-                            </Tab>
-                        </Tabs>
-                    </CardBody>
+                    <CardBody>{details}</CardBody>
                 </Card>
             </PageSection>
         </div>
     );
 };
+
+const ProjectLink = (props) => {
+    const handleClick = () => window.open(props.link, "_blank");
+    if (props.link === "") {
+        return "";
+    }
+    return <ExternalLinkAltIcon onClick={handleClick} />;
+};
+
 export { ProjectInfo };
