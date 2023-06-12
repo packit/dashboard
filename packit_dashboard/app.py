168f979aba6baec08d9f2665a69a19a1e6450926
Jiri Popelka <jpopelka@redhat.com>
2023-06-12 12:34:11 +0200

    Don't run Flask app in debug mode on prod
    https://cwe.mitre.org/data/definitions/215.html
    https://cwe.mitre.org/data/definitions/489.html

--- packit_dashboard/app.py
+++ packit_dashboard/app.py
@@ -1,6 +1,9 @@
+from os import getenv
+
 from flask import Flask
-from packit_dashboard.home.routes import home
+
 from packit_dashboard.api.routes import api
+from packit_dashboard.home.routes import home
 
 app = Flask(
     "Packit Service Dashboard",
@@ -8,10 +11,10 @@ app = Flask(
 )
 
 app.register_blueprint(api)
-# Note: Declare any other flask blueprints or routes above this
+# Note: Declare any other flask blueprints or routes above this.
 # Routes declared below this will be rendered by React
 app.register_blueprint(home)
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=getenv("DEPLOYMENT") != "prod")
