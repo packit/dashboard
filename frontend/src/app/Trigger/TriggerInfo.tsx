f1e2844b888fe260734b0e601ad638dddf2b12ca
SpyTec <eric@spytec.se>
2023-10-15 18:50:24 +0200

    fix: TriggerInfo to use PatternFly table
    Currently TriggerInfo uses the PatternFly classes for the table but
    not the table itself. This changes it to use the PF component

--- frontend/src/app/Trigger/TriggerInfo.tsx
+++ frontend/src/app/Trigger/TriggerInfo.tsx
@@ -8,6 +8,14 @@ import {
 } from "@patternfly/react-core";
 
 import { CaretDownIcon, ExternalLinkAltIcon } from "@patternfly/react-icons";
+import {
+    TableComposable,
+    Tbody,
+    Td,
+    Th,
+    Thead,
+    Tr,
+} from "@patternfly/react-table";
 
 interface TriggerInfoProps {
     trigger: {
@@ -55,139 +63,92 @@ const TriggerInfo: React.FC<TriggerInfoProps> = (props) => {
     let activeViewContent;
     if (activeView === "Builds") {
         activeViewContent = (
-            <div>
-                <table
-                    className="pf-c-table pf-m-compact pf-m-grid-md"
-                    role="grid"
-                    aria-label="Builds Table"
-                >
-                    <thead>
-                        <tr role="row">
-                            <th role="columnheader" scope="col">
-                                Build ID
-                            </th>
-                            <th role="columnheader" scope="col">
-                                Chroot
-                            </th>
-                            <th role="columnheader" scope="col">
-                                Status
-                            </th>
-                            <th role="columnheader" scope="col">
-                                Web URL
-                            </th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {props.trigger.builds.map((build, index) => (
-                            <tr role="row" key={index}>
-                                <td role="cell" data-label="Build ID">
-                                    {build.build_id}
-                                </td>
-                                <td role="cell" data-label="Chroot">
-                                    <Label color="blue">{build.chroot}</Label>
-                                </td>
-                                <td role="cell" data-label="Status">
-                                    {build.status}
-                                </td>
-                                <td role="cell" data-label="Web URL">
-                                    <WebUrlIcon link={build.web_url} />
-                                </td>
-                            </tr>
-                        ))}
-                    </tbody>
-                </table>
-            </div>
+            <TableComposable variant="compact" aria-label="Builds Table">
+                <Thead>
+                    <Tr>
+                        <Th>Build ID</Th>
+                        <Th>Chroot</Th>
+                        <Th>Success</Th>
+                        <Th>Web URL</Th>
+                    </Tr>
+                </Thead>
+                <Tbody>
+                    {props.trigger.builds.map((build, index) => (
+                        <Tr role="row" key={index}>
+                            <Td data-label="Build ID">{build.build_id}</Td>
+                            <Td data-label="Chroot">
+                                <Label color="blue">{build.chroot}</Label>
+                            </Td>
+                            <Td data-label="Status">{build.status}</Td>
+                            <Td data-label="Web URL">
+                                <WebUrlIcon link={build.web_url} />
+                            </Td>
+                        </Tr>
+                    ))}
+                </Tbody>
+            </TableComposable>
         );
     }
     if (activeView === "SRPM Builds") {
         activeViewContent = (
-            <div>
-                <table
-                    className="pf-c-table pf-m-compact pf-m-grid-md"
-                    role="grid"
-                    aria-label="SRPM Builds Table"
-                >
-                    <thead>
-                        <tr role="row">
-                            <th role="columnheader" scope="col">
-                                SRPM Build ID
-                            </th>
-                            <th role="columnheader" scope="col">
-                                Success
-                            </th>
-                            <th role="columnheader" scope="col">
-                                Logs
-                            </th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {props.trigger.srpm_builds.map((build, index) => (
-                            <tr role="row" key={index}>
-                                <td role="cell" data-label="SRPM Build ID">
-                                    {build.srpm_build_id}
-                                </td>
-                                <td role="cell" data-label="Status">
-                                    {build.status}
-                                </td>
-                                <td role="cell" data-label="Logs">
-                                    <WebUrlIcon link={build.log_url} />
-                                </td>
-                            </tr>
-                        ))}
-                    </tbody>
-                </table>
-            </div>
+            <TableComposable variant="compact" aria-label="SRPM Builds Table">
+                <Thead>
+                    <Tr>
+                        <Th>SRPM Build ID</Th>
+                        <Th>Success</Th>
+                        <Th>Logs</Th>
+                    </Tr>
+                </Thead>
+                <Tbody>
+                    {props.trigger.srpm_builds.map((build, index) => (
+                        <Tr key={index}>
+                            <Td data-label="SRPM Build ID">
+                                {build.srpm_build_id}
+                            </Td>
+                            <Td data-label="Status">{build.status}</Td>
+                            <Td data-label="Logs">
+                                <WebUrlIcon link={build.log_url} />
+                            </Td>
+                        </Tr>
+                    ))}
+                </Tbody>
+            </TableComposable>
         );
     }
     if (activeView === "Test Runs") {
         activeViewContent = (
-            <div>
-                <table
-                    className="pf-c-table pf-m-compact pf-m-grid-md"
-                    role="grid"
-                    aria-label="Testing Farm Results Table"
-                >
-                    <thead>
-                        <tr role="row">
-                            <th role="columnheader" scope="col">
-                                Pipeline ID
-                            </th>
-                            <th role="columnheader" scope="col">
-                                Chroot
-                            </th>
-                            <th role="columnheader" scope="col">
-                                Status
-                            </th>
-                            <th role="columnheader" scope="col">
-                                Web URL
-                            </th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {props.trigger.tests.map((test, index) => (
-                            <tr role="row">
-                                <td role="cell" data-label="Pipeline ID">
-                                    {test.pipeline_id}
-                                </td>
-                                <td role="cell" data-label="Chroot">
-                                    <Label color="blue">{test.chroot}</Label>
-                                </td>
-                                <td role="cell" data-label="Status">
-                                    {test.status}
-                                </td>
-                                <td role="cell" data-label="Web URL">
-                                    <WebUrlIcon link={test.web_url} />
-                                </td>
-                            </tr>
-                        ))}
-                    </tbody>
-                </table>
-            </div>
+            <TableComposable
+                variant="compact"
+                aria-label="Testing Farm Results Table"
+            >
+                <Thead>
+                    <Tr>
+                        <Th>Pipeline ID</Th>
+                        <Th>Chroot</Th>
+                        <Th>Status</Th>
+                        <Th>Web URL</Th>
+                    </Tr>
+                </Thead>
+                <Tbody>
+                    {props.trigger.tests.map((test) => (
+                        <Tr>
+                            <Td data-label="Pipeline ID">{test.pipeline_id}</Td>
+                            <Td data-label="Chroot">
+                                <Label color="blue">{test.chroot}</Label>
+                            </Td>
+                            <Td data-label="Status">{test.status}</Td>
+                            <Td data-label="Web URL">
+                                <WebUrlIcon link={test.web_url} />
+                            </Td>
+                        </Tr>
+                    ))}
+                </Tbody>
+            </TableComposable>
         );
     }
 
     return (
-        <div>
+        <>
             <Dropdown
                 onSelect={onToggle}
                 toggle={
@@ -224,7 +185,7 @@ const TriggerInfo: React.FC<TriggerInfoProps> = (props) => {
             <br />
 
             {activeViewContent}
-        </div>
+        </>
     );
 };
 
