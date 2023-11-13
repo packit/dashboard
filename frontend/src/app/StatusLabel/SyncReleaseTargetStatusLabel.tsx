7da28b0466200933c39a183cf88f12166d9540d9
Maja Massarini <mmassari@redhat.com>
2023-11-13 13:59:46 +0100

    Visualize skipped sync releases

--- frontend/src/app/StatusLabel/SyncReleaseTargetStatusLabel.tsx
+++ frontend/src/app/StatusLabel/SyncReleaseTargetStatusLabel.tsx
@@ -38,6 +38,10 @@ export const SyncReleaseTargetStatusLabel: React.FC<StatusLabelProps> = (
                 setColor("green");
                 setIcon(<CheckCircleIcon />);
                 break;
+            case "skipped":
+                setColor("gray");
+                setIcon(<CheckCircleIcon />);
+                break;
         }
     }, [props.status]);
 
