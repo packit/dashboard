160bf3a05a885f0bbb18de705f12dd5d757cc612
Maja Massarini <mmassari@redhat.com>
2024-05-06 11:47:39 +0200

    Suggest retry when loading statistics

--- frontend/src/app/Usage/UsageInterval.tsx
+++ frontend/src/app/Usage/UsageInterval.tsx
@@ -71,7 +71,9 @@ const UsageInterval: React.FC<UsageIntervalProps> = (props) => {
         <Card>
           <CardBody>
             <Title headingLevel="h1" size="lg">
-              Not Found.
+              {!data
+                ? "Data is loading, try again in few minutes."
+                : "Not Found."}
             </Title>
           </CardBody>
         </Card>
