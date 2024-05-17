3aa548f3725623c872ddf0e4e5d603ece67f0bda
Freya Gustavsson <freya@spytec.se>
2024-05-17 16:40:14 +0200

    feat: Added Sentry monitoring
    This adds Sentry integration to the dashboard which will report any
    errors or issues. In the future, we can add a modal as well where the
    user can provide their own feedback.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,10 +3,24 @@
 
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig(() => ({
-  plugins: [react()],
+  build: {
+    sourcemap: true,
+  },
+  plugins: [
+    react(),
+    // Put the Sentry vite plugin after all other plugins
+    sentryVitePlugin({
+      org: "red-hat-it",
+      project: "pckt-002-packit-service",
+      authToken: process.env.SENTRY_AUTH_TOKEN,
+      telemetry: false,
+      reactComponentAnnotation: { enabled: true },
+    }),
+  ],
   esbuild: {
     loader: "tsx",
     include: /src\/.*\.[tj]sx?$/,
