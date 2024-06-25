06d5ce91592d985e3d7563ecd01f22cef62cdad1
Freya Gustavsson <freya@spytec.se>
2024-06-25 18:46:29 +0200

    perf: Minimize images as they are used in the app
    We have the images already in the source code but we do not minimize
    depending on where we use them. This takes care of that
    
    We also now split the JS files a bit to improve fetching, but it is not
    a performance improvement as they all will still load at the same time

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,14 +4,38 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { sentryVitePlugin } from "@sentry/vite-plugin";
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 
 // https://vitejs.dev/config/
 export default defineConfig(() => ({
   build: {
     sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: function manualChunks(id) {
+          if (
+            id.includes("victory-") ||
+            id.includes("d3") ||
+            id.includes("@patternfly/react-charts")
+          ) {
+            return "charts";
+          } else if (id.includes("@patternfly")) {
+            return "patternfly";
+          } else if (id.includes("node_modules")) {
+            return "vendor";
+          }
+        },
+      },
+    },
+    esbuild: {
+      loader: "tsx",
+      include: /src\/.*\.[tj]sx?$/,
+      exclude: [],
+    },
   },
   plugins: [
     react(),
+    ViteImageOptimizer(),
     // Put the Sentry vite plugin after all other plugins
     sentryVitePlugin({
       org: "red-hat-it",
@@ -21,11 +45,6 @@ export default defineConfig(() => ({
       reactComponentAnnotation: { enabled: true },
     }),
   ],
-  esbuild: {
-    loader: "tsx",
-    include: /src\/.*\.[tj]sx?$/,
-    exclude: [],
-  },
   server: {
     open: true,
   },
