7a83a3d0b6462310004cd736cfcd3cf0f3061bda
Freya Gustavsson <freya@spytec.se>
2024-07-31 23:55:52 +0200

    Fixed Copr table and detailed view

--- frontend/src/routes/jobs_/copr/$id.tsx
+++ frontend/src/routes/jobs_/copr/$id.tsx
@@ -3,7 +3,10 @@
 
 import { createFileRoute } from "@tanstack/react-router";
 import { CoprBuild } from "../../../components/copr/CoprBuild";
+import { coprBuildQueryOptions } from "../../../queries/copr/coprQuery";
 
 export const Route = createFileRoute("/jobs/copr/$id")({
+  loader: ({ context: { queryClient }, params: { id } }) =>
+    queryClient.ensureQueryData(coprBuildQueryOptions({ id })),
   component: CoprBuild,
 });
