ca2877724b4b7439430769ec843eade073d8538c
Freya Gustavsson <freya@spytec.se>
2024-08-01 15:57:29 +0200

    Upgraded Koji table

--- frontend/src/components/koji/KojiBuildsTable.tsx
+++ frontend/src/components/koji/KojiBuildsTable.tsx
@@ -3,119 +3,106 @@
 
 import React, { useMemo } from "react";
 
-import { TableVariant, cellWidth } from "@patternfly/react-table";
 import {
   Table,
-  TableHeader,
-  TableBody,
-} from "@patternfly/react-table/deprecated";
+  TableVariant,
+  Tbody,
+  Td,
+  Th,
+  Thead,
+  Tr,
+} from "@patternfly/react-table";
 
 import {
   TriggerLink,
   TriggerSuffix,
 } from "../../components/trigger/TriggerLink";
-import { ErrorConnection } from "../../components/errors/ErrorConnection";
-import { Preloader } from "../../components/shared/Preloader";
 import { Timestamp } from "../../components/shared/Timestamp";
 import { useInfiniteQuery } from "@tanstack/react-query";
 import { kojiBuildsQueryOptions } from "../../queries/koji/kojiBuildsQuery";
 import { StatusLabel } from "../statusLabels/StatusLabel";
-import { KojiBuild } from "../../apiDefinitions";
 import { ForgeIcon } from "../icons/ForgeIcon";
 import { LoadMore } from "../shared/LoadMore";
+import { SkeletonTable } from "@patternfly/react-component-groups";
 
 interface KojiBuildTableProps {
   scratch: boolean;
 }
-const KojiBuildsTable: React.FC<KojiBuildTableProps> = ({ scratch }) => {
+export const KojiBuildsTable: React.FC<KojiBuildTableProps> = ({ scratch }) => {
+  const { data, isLoading, isFetchingNextPage, hasNextPage, fetchNextPage } =
+    useInfiniteQuery(kojiBuildsQueryOptions({ scratch }));
+
   // Headings
-  const columns = [
-    {
-      title: <span className="pf-v5-u-screen-reader">Forge</span>,
-    }, // space for forge icon
-    { title: "Trigger", transforms: [cellWidth(35)] },
-    { title: "Target", transforms: [cellWidth(20)] },
-    { title: "Time Submitted", transforms: [cellWidth(20)] },
-    { title: "Koji Build Task", transforms: [cellWidth(20)] },
-  ];
-
-  const {
-    isError,
-    data,
-    isLoading,
-    isFetchingNextPage,
-    hasNextPage,
-    fetchNextPage,
-  } = useInfiniteQuery(kojiBuildsQueryOptions({ scratch }));
-
-  // Convert fetched json into row format that the table can read
-  function jsonToRow(koji_build: KojiBuild) {
-    return {
-      cells: [
-        {
-          title: <ForgeIcon url={koji_build.project_url} />,
-        },
-        {
-          title: (
-            <strong>
-              <TriggerLink trigger={koji_build}>
-                <TriggerSuffix trigger={koji_build} />
-              </TriggerLink>
-            </strong>
-          ),
-        },
-        {
-          title: (
-            <StatusLabel
-              target={koji_build.chroot}
-              status={koji_build.status}
-              link={`/results/koji-builds/${koji_build.packit_id}`}
-            />
-          ),
-        },
-        {
-          title: <Timestamp stamp={koji_build.build_submitted_time} />,
-        },
-        {
-          title: (
-            <strong>
-              <a href={koji_build.web_url} target="_blank" rel="noreferrer">
-                {koji_build.task_id}
-              </a>
-            </strong>
-          ),
-        },
-      ],
-    };
-  }
+  const columnNames = {
+    forge: "Forge",
+    trigger: "Trigger",
+    target: "Target",
+    timeSubmitted: "Time Submitted",
+    kojiBuildTask: "Koji Build Task",
+  };
 
   // Create a memoization of all the data when we flatten it out. Ideally one should render all the pages separately so that rendering will be done faster
-  const rows = useMemo(
-    () => (data ? data.pages.flat().map(jsonToRow) : []),
-    [data],
-  );
+  const rows = useMemo(() => (data ? data.pages.flat() : []), [data]);
 
-  // If backend API is down
-  if (isError) {
-    return <ErrorConnection />;
-  }
+  const TableHeads = [
+    <Th>
+      <span className="pf-v5-u-screen-reader">{columnNames.forge}</span>
+    </Th>,
+    <Th width={35}>{columnNames.trigger}</Th>,
+    <Th width={20}>{columnNames.target}</Th>,
+    <Th width={20}>{columnNames.timeSubmitted}</Th>,
+    <Th width={20}>{columnNames.kojiBuildTask}</Th>,
+  ];
 
-  // Show preloader if waiting for API data
-  // TODO(SpyTec): Replace with skeleton loader, we know the data will look like
   if (isLoading) {
-    return <Preloader />;
+    return (
+      <SkeletonTable
+        variant={TableVariant.compact}
+        rows={10}
+        columns={TableHeads}
+      />
+    );
   }
 
   return (
     <>
-      <Table
-        aria-label="Koji builds"
-        variant={TableVariant.compact}
-        cells={columns}
-        rows={rows}
-      >
-        <TableHeader />
-        <TableBody />
+      <Table aria-label="Koji builds" variant={TableVariant.compact}>
+        <Thead>
+          <Tr>{TableHeads}</Tr>
+        </Thead>
+        <Tbody>
+          {rows.map((koji_build) => (
+            <Tr key={koji_build.task_id}>
+              <Td dataLabel={columnNames.forge}>
+                <ForgeIcon url={koji_build.project_url} />
+              </Td>
+              <Td dataLabel={columnNames.trigger}>
+                <strong>
+                  <TriggerLink trigger={koji_build}>
+                    <TriggerSuffix trigger={koji_build} />
+                  </TriggerLink>
+                </strong>
+              </Td>
+              <Td dataLabel={columnNames.trigger}>
+                <StatusLabel
+                  target={koji_build.chroot}
+                  status={koji_build.status}
+                  link={`/results/koji-builds/${koji_build.packit_id}`}
+                />
+              </Td>
+              <Td dataLabel={columnNames.timeSubmitted}>
+                <Timestamp stamp={koji_build.build_submitted_time} />
+              </Td>
+              <Td dataLabel={columnNames.kojiBuildTask}>
+                <strong>
+                  <a href={koji_build.web_url} target="_blank" rel="noreferrer">
+                    {koji_build.task_id}
+                  </a>
+                </strong>
+              </Td>
+            </Tr>
+          ))}
+        </Tbody>
       </Table>
       <LoadMore
         isFetchingNextPage={isFetchingNextPage}
@@ -125,5 +112,3 @@ const KojiBuildsTable: React.FC<KojiBuildTableProps> = ({ scratch }) => {
     </>
   );
 };
-
-export { KojiBuildsTable };
