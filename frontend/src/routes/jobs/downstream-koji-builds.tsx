b212e93cf57d49bca81604a7096c6304f04c86ba
Freya Gustavsson <freya@spytec.se>
2024-08-07 13:52:00 +0200

    Refactored downstream koji table

---
+++ frontend/src/routes/jobs/downstream-koji-builds.tsx
@@ -0,0 +1,15 @@
+// Copyright Contributors to the Packit project.
+// SPDX-License-Identifier: MIT
+
+import { createFileRoute, redirect } from "@tanstack/react-router";
+
+export const Route = createFileRoute("/jobs/downstream-koji-builds")({
+  loader: ({ location }) => {
+    throw redirect({
+      to: location.pathname.replace(
+        "/downstream-koji-builds",
+        "/koji/downstream",
+      ),
+    });
+  },
+});
