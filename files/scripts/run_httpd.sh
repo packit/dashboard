c312c03c3cc73b18cedb72c26cd07e8c1b08c11b
Matej Focko <mfocko@redhat.com>
2025-01-17 14:11:38 +0100

    fix(wsgi): alias the API server name
    Fixes packit/packit-service#2529
    
    Signed-off-by: Matej Focko <mfocko@redhat.com>

--- files/scripts/run_httpd.sh
+++ files/scripts/run_httpd.sh
@@ -10,6 +10,10 @@ else
     SERVER_NAME="dashboard.${DEPLOYMENT}.packit.dev"
 fi
 
+# [FIXME] This is a dirty hack to workaround 403 by the httpd server itself when
+# open HTTP/2 connection gets reused
+API_SERVER_NAME="${DEPLOYMENT}.packit.dev"
+
 # See "mod_wsgi-express-3 start-server --help" for details on
 # these options, and the configuration documentation of mod_wsgi:
 # https://modwsgi.readthedocs.io/en/master/configuration.html
@@ -21,6 +25,7 @@ exec mod_wsgi-express-3 start-server \
     --ssl-certificate-file /secrets/fullchain.pem \
     --ssl-certificate-key-file /secrets/privkey.pem \
     --server-name ${SERVER_NAME} \
+    --server-alias ${API_SERVER_NAME} \
     --processes 2 \
     --locale "C.UTF-8" \
     /usr/share/packit_dashboard/packit_dashboard.wsgi
