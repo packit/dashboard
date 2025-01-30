4899a05c347d4ca6b5de0f8423e04632506b837f
Freya Gustavsson <freya@venefilyn.se>
2025-01-30 13:45:36 +0100

    SRPM: Migrate to page-based pagination

--- frontend/src/components/srpm/SRPMBuildsTable.tsx
+++ frontend/src/components/srpm/SRPMBuildsTable.tsx
@@ -1,7 +1,7 @@
 // Copyright Contributors to the Packit project.
 // SPDX-License-Identifier: MIT
 
-import { useMemo } from "react";
+import { useState } from "react";
 
 import {
   Table,
@@ -14,24 +14,24 @@ import {
 } from "@patternfly/react-table";
 
 import { SkeletonTable } from "@patternfly/react-component-groups";
-import { useInfiniteQuery } from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 import { srpmBuildsQueryOptions } from "../../queries/srpm/srpmBuildsQuery";
 import { ErrorConnection } from "../errors/ErrorConnection";
 import { ForgeIcon } from "../icons/ForgeIcon";
-import { LoadMore } from "../shared/LoadMore";
+import { PackitPagination } from "../shared/PackitPagination";
+import { PackitPaginationContext } from "../shared/PackitPaginationContext";
 import { Timestamp } from "../shared/Timestamp";
 import { StatusLabel } from "../statusLabels/StatusLabel";
 import { TriggerLink, TriggerSuffix } from "../trigger/TriggerLink";
 
 export const SRPMBuildsTable = () => {
-  const {
-    isLoading,
-    isError,
-    data,
-    isFetchingNextPage,
-    hasNextPage,
-    fetchNextPage,
-  } = useInfiniteQuery(srpmBuildsQueryOptions());
+  const [page, setPage] = useState(1);
+  const [perPage, setPerPage] = useState(10);
+  const value = { page, setPage, perPage, setPerPage };
+
+  const { isLoading, isError, data } = useQuery(
+    srpmBuildsQueryOptions(page, perPage),
+  );
 
   // Headings
   const columnNames = {
@@ -40,9 +40,6 @@ export const SRPMBuildsTable = () => {
     results: "Results",
     timeSubmitted: "Time Submitted",
   };
-
-  // Create a memoization of all the data when we flatten it out. Ideally one should render all the pages separately so that rendering will be done faster
-  const rows = useMemo(() => (data ? data.pages.flat() : []), [data]);
 
   const TableHeads = [
     <Th key={columnNames.forge}>
@@ -64,53 +61,47 @@ export const SRPMBuildsTable = () => {
     return <ErrorConnection />;
   }
 
-  if (isLoading) {
-    return (
-      <SkeletonTable
-        variant={TableVariant.compact}
-        rows={10}
-        columns={TableHeads}
-      />
-    );
-  }
-
   return (
-    <>
-      <Table aria-label="SRPM builds" variant={TableVariant.compact}>
-        <Thead>
-          <Tr>{TableHeads}</Tr>
-        </Thead>
-        <Tbody>
-          {rows.map((srpm_build) => (
-            <Tr key={srpm_build.srpm_build_id}>
-              <Td dataLabel={columnNames.forge}>
-                <ForgeIcon url={srpm_build.project_url} />
-              </Td>
-              <Td dataLabel={columnNames.trigger}>
-                <strong>
-                  <TriggerLink trigger={srpm_build}>
-                    <TriggerSuffix trigger={srpm_build} />
-                  </TriggerLink>
-                </strong>
-              </Td>
-              <Td dataLabel={columnNames.results}>
-                <StatusLabel
-                  status={srpm_build.status}
-                  link={`/jobs/srpm/${srpm_build.srpm_build_id}`}
-                />
-              </Td>
-              <Td dataLabel={columnNames.timeSubmitted}>
-                <Timestamp stamp={srpm_build.build_submitted_time} />
-              </Td>
-            </Tr>
-          ))}
-        </Tbody>
-      </Table>
-      <LoadMore
-        isFetchingNextPage={isFetchingNextPage}
-        hasNextPage={hasNextPage}
-        fetchNextPage={() => void fetchNextPage()}
-      />
-    </>
+    <PackitPaginationContext.Provider value={value}>
+      <PackitPagination />
+      {isLoading ? (
+        <SkeletonTable
+          variant={TableVariant.compact}
+          rowsCount={perPage}
+          columns={TableHeads}
+        />
+      ) : (
+        <Table aria-label="SRPM builds" variant={TableVariant.compact}>
+          <Thead>
+            <Tr>{TableHeads}</Tr>
+          </Thead>
+          <Tbody>
+            {data?.map((srpm_build) => (
+              <Tr key={srpm_build.srpm_build_id}>
+                <Td dataLabel={columnNames.forge}>
+                  <ForgeIcon url={srpm_build.project_url} />
+                </Td>
+                <Td dataLabel={columnNames.trigger}>
+                  <strong>
+                    <TriggerLink trigger={srpm_build}>
+                      <TriggerSuffix trigger={srpm_build} />
+                    </TriggerLink>
+                  </strong>
+                </Td>
+                <Td dataLabel={columnNames.results}>
+                  <StatusLabel
+                    status={srpm_build.status}
+                    link={`/jobs/srpm/${srpm_build.srpm_build_id}`}
+                  />
+                </Td>
+                <Td dataLabel={columnNames.timeSubmitted}>
+                  <Timestamp stamp={srpm_build.build_submitted_time} />
+                </Td>
+              </Tr>
+            ))}
+          </Tbody>
+        </Table>
+      )}
+    </PackitPaginationContext.Provider>
   );
 };
--- frontend/src/queries/srpm/srpmBuilds.ts
+++ frontend/src/queries/srpm/srpmBuilds.ts
@@ -5,16 +5,18 @@ import { SRPMBuildGroup } from "../../apiDefinitions";
 
 export interface fetchSRPMBuildsProps {
   pageParam: number;
+  perPage: number;
   signal?: AbortSignal;
 }
 
 // Fetch data from dashboard backend (or if we want, directly from the API)
 export const fetchSRPMBuilds = async ({
   pageParam = 1,
+  perPage,
   signal,
 }: fetchSRPMBuildsProps): Promise<SRPMBuildGroup[]> => {
   const data = await fetch(
-    `${import.meta.env.VITE_API_URL}/srpm-builds?page=${pageParam}`,
+    `${import.meta.env.VITE_API_URL}/srpm-builds?page=${pageParam}&per_page=${perPage}`,
     { signal },
   )
     .then((response) => response.json())
