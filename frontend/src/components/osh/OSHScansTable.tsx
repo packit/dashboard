c59a6539965d0fe2c087d12454b1ca5bfd981a01
Freya Gustavsson <freya@venefilyn.se>
2025-01-30 13:58:42 +0100

    OpenScanHub: Migrate to page-based pagination

--- frontend/src/components/osh/OSHScansTable.tsx
+++ frontend/src/components/osh/OSHScansTable.tsx
@@ -1,7 +1,7 @@
 // Copyright Contributors to the Packit project.
 // SPDX-License-Identifier: MIT
 
-import { useMemo } from "react";
+import { useState } from "react";
 
 import {
   Table,
@@ -14,7 +14,7 @@ import {
 } from "@patternfly/react-table";
 
 import { SkeletonTable } from "@patternfly/react-component-groups";
-import { useInfiniteQuery } from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 import { ErrorConnection } from "../../components/errors/ErrorConnection";
 import { Timestamp } from "../../components/shared/Timestamp";
 import {
@@ -23,10 +23,15 @@ import {
 } from "../../components/trigger/TriggerLink";
 import { oshScansQueryOptions } from "../../queries/osh/oshScansQuery";
 import { ForgeIcon } from "../icons/ForgeIcon";
-import { LoadMore } from "../shared/LoadMore";
+import { PackitPagination } from "../shared/PackitPagination";
+import { PackitPaginationContext } from "../shared/PackitPaginationContext";
 import { StatusLabel } from "../statusLabels/StatusLabel";
 
 const OSHScansTable = () => {
+  const [page, setPage] = useState(1);
+  const [perPage, setPerPage] = useState(10);
+  const value = { page, setPage, perPage, setPerPage };
+
   // Headings
   const columnNames = {
     forge: "Forge",
@@ -37,17 +42,9 @@ const OSHScansTable = () => {
     newFindings: "New findings",
   };
 
-  const {
-    isLoading,
-    isError,
-    fetchNextPage,
-    data,
-    isFetchingNextPage,
-    hasNextPage,
-  } = useInfiniteQuery(oshScansQueryOptions());
-
-  // Create a memoization of all the data when we flatten it out. Ideally one should render all the pages separately so that rendering will be done faster
-  const rows = useMemo(() => (data ? data.pages.flat() : []), [data]);
+  const { isLoading, isError, data } = useQuery(
+    oshScansQueryOptions(page, perPage),
+  );
 
   const TableHeads = [
     <Th key={columnNames.forge}>
@@ -75,65 +72,59 @@ const OSHScansTable = () => {
     return <ErrorConnection />;
   }
 
-  if (isLoading) {
-    return (
-      <SkeletonTable
-        variant={TableVariant.compact}
-        rowsCount={10}
-        columns={TableHeads}
-      />
-    );
-  }
-
   return (
-    <>
-      <Table aria-label="OpenScanHub scans" variant={TableVariant.compact}>
-        <Thead>
-          <Tr>{TableHeads}</Tr>
-        </Thead>
-        <Tbody>
-          {rows.map((scan) => (
-            <Tr key={scan.packit_id}>
-              <Td dataLabel={columnNames.forge}>
-                <ForgeIcon url={scan.project_url} />
-              </Td>
-              <Td dataLabel={columnNames.trigger}>
-                <strong>
-                  <TriggerLink trigger={scan}>
-                    <TriggerSuffix trigger={scan} />
-                  </TriggerLink>
-                </strong>
-              </Td>
-              <Td dataLabel={columnNames.scanDetails}>
-                <StatusLabel
-                  target={"rawhide"}
-                  status={scan.status}
-                  link={`/jobs/openscanhub/${scan.packit_id}`}
-                />
-              </Td>
-              <Td dataLabel={columnNames.newFindings}>
-                {scan.issues_added_count ?? "N/A"}
-              </Td>
-              <Td dataLabel={columnNames.timeProcessed}>
-                <Timestamp stamp={scan.submitted_time} />
-              </Td>
-              <Td dataLabel={columnNames.scan}>
-                <strong>
-                  <a href={scan.url ?? ""} target="_blank" rel="noreferrer">
-                    {scan.task_id}
-                  </a>
-                </strong>
-              </Td>
-            </Tr>
-          ))}
-        </Tbody>
-      </Table>
-      <LoadMore
-        isFetchingNextPage={isFetchingNextPage}
-        hasNextPage={hasNextPage}
-        fetchNextPage={() => void fetchNextPage()}
-      />
-    </>
+    <PackitPaginationContext.Provider value={value}>
+      <PackitPagination />
+      {isLoading ? (
+        <SkeletonTable
+          variant={TableVariant.compact}
+          rowsCount={perPage}
+          columns={TableHeads}
+        />
+      ) : (
+        <Table aria-label="OpenScanHub scans" variant={TableVariant.compact}>
+          <Thead>
+            <Tr>{TableHeads}</Tr>
+          </Thead>
+          <Tbody>
+            {data?.map((scan) => (
+              <Tr key={scan.packit_id}>
+                <Td dataLabel={columnNames.forge}>
+                  <ForgeIcon url={scan.project_url} />
+                </Td>
+                <Td dataLabel={columnNames.trigger}>
+                  <strong>
+                    <TriggerLink trigger={scan}>
+                      <TriggerSuffix trigger={scan} />
+                    </TriggerLink>
+                  </strong>
+                </Td>
+                <Td dataLabel={columnNames.scanDetails}>
+                  <StatusLabel
+                    target={"rawhide"}
+                    status={scan.status}
+                    link={`/jobs/openscanhub/${scan.packit_id}`}
+                  />
+                </Td>
+                <Td dataLabel={columnNames.newFindings}>
+                  {scan.issues_added_count ?? "N/A"}
+                </Td>
+                <Td dataLabel={columnNames.timeProcessed}>
+                  <Timestamp stamp={scan.submitted_time} />
+                </Td>
+                <Td dataLabel={columnNames.scan}>
+                  <strong>
+                    <a href={scan.url ?? ""} target="_blank" rel="noreferrer">
+                      {scan.task_id}
+                    </a>
+                  </strong>
+                </Td>
+              </Tr>
+            ))}
+          </Tbody>
+        </Table>
+      )}
+    </PackitPaginationContext.Provider>
   );
 };
 
--- frontend/src/queries/osh/oshScans.ts
+++ frontend/src/queries/osh/oshScans.ts
@@ -5,16 +5,18 @@ import { OSHScanGroup } from "../../apiDefinitions";
 
 export interface fetchScansProps {
   pageParam: number;
+  perPage: number;
   signal?: AbortSignal;
 }
 
 // Fetch data from dashboard backend (or if we want, directly from the API)
 export const fetchScans = async ({
   pageParam = 1,
+  perPage,
   signal,
 }: fetchScansProps): Promise<OSHScanGroup[]> => {
   const data = await fetch(
-    `${import.meta.env.VITE_API_URL}/openscanhub-scans?page=${pageParam}`,
+    `${import.meta.env.VITE_API_URL}/openscanhub-scans?page=${pageParam}&per_page=${perPage}`,
     { signal },
   )
     .then((response) => response.json())
