0917e5f4576de5c73d300801ec9f785912bc3928
Freya Gustavsson <freya@venefilyn.se>
2025-01-30 13:58:42 +0100

    OpenScanHub: Migrate to page-based pagination

--- frontend/src/queries/osh/oshScansQuery.ts
+++ frontend/src/queries/osh/oshScansQuery.ts
@@ -1,25 +1,12 @@
 // Copyright Contributors to the Packit project.
 // SPDX-License-Identifier: MIT
 
-import { infiniteQueryOptions } from "@tanstack/react-query";
+import { queryOptions } from "@tanstack/react-query";
 import { fetchScans } from "./oshScans";
 
-export const oshScansQueryOptions = () =>
-  infiniteQueryOptions({
-    queryKey: ["openscanhub"],
-    queryFn: async ({ pageParam, signal }) =>
-      await fetchScans({ pageParam, signal }),
-    initialPageParam: 1,
-    getNextPageParam: (lastPage, _allPages, lastPageParam) => {
-      if (lastPage.length === 0) {
-        return undefined;
-      }
-      return lastPageParam + 1;
-    },
-    getPreviousPageParam: (_firstPage, _allPages, firstPageParam) => {
-      if (firstPageParam <= 1) {
-        return undefined;
-      }
-      return firstPageParam - 1;
-    },
+export const oshScansQueryOptions = (pageParam: number, perPage: number = 20) =>
+  queryOptions({
+    queryKey: ["openscanhub", { pageParam, perPage }],
+    queryFn: async ({ signal }) =>
+      await fetchScans({ pageParam, perPage, signal }),
   });
