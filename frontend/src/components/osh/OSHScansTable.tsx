860d8328c525a7d54a0a0330ebdc4780db815ea2
Alzbeta Kucerova <akucerov@redhat.com>
2025-11-07 10:46:05 +0100

    Add OSH results link to dashboard
    A new column has been added to the OSH dashboard, which contains
    the link to OSH results. Columns have been resized appropriately
    and column headers have been capitalized for consistency.

--- frontend/src/components/osh/OSHScansTable.tsx
+++ frontend/src/components/osh/OSHScansTable.tsx
@@ -27,6 +27,10 @@ import { PackitPagination } from "../shared/PackitPagination";
 import { PackitPaginationContext } from "../shared/PackitPaginationContext";
 import { StatusLabel } from "../statusLabels/StatusLabel";
 
+function getHTMLScanUrl(scanUrl: string): string {
+  return scanUrl?.replace(".js?format=raw", ".html");
+}
+
 const OSHScansTable = () => {
   const [page, setPage] = useState(1);
   const [perPage, setPerPage] = useState(10);
@@ -36,10 +40,11 @@ const OSHScansTable = () => {
   const columnNames = {
     forge: "Forge",
     trigger: "Trigger",
-    scanDetails: "Scan details",
+    scanDetails: "Scan Details",
     timeProcessed: "Time Processed",
-    scan: "OpenScanHub task",
-    newFindings: "New findings",
+    scan: "OpenScanHub Task",
+    allFindings: "All Findings",
+    newFindings: "New Findings",
   };
 
   const { isLoading, isError, data } = useQuery(
@@ -48,7 +53,7 @@ const OSHScansTable = () => {
 
   const TableHeads = [
     <Th key={columnNames.forge} screenReaderText={columnNames.forge}></Th>,
-    <Th key={columnNames.trigger} width={35}>
+    <Th key={columnNames.trigger} width={20}>
       {columnNames.trigger}
     </Th>,
     <Th key={columnNames.scanDetails} width={20}>
@@ -57,11 +62,14 @@ const OSHScansTable = () => {
     <Th key={columnNames.newFindings} width={15}>
       {columnNames.newFindings}
     </Th>,
-    <Th key={columnNames.timeProcessed} width={20}>
+    <Th key={columnNames.timeProcessed} width={10}>
       {columnNames.timeProcessed}
     </Th>,
-    <Th key={columnNames.scan} width={20}>
+    <Th key={columnNames.scan} width={15}>
       {columnNames.scan}
+    </Th>,
+    <Th key={columnNames.allFindings} width={15}>
+      {columnNames.allFindings}
     </Th>,
   ];
 
@@ -116,6 +124,19 @@ const OSHScansTable = () => {
                       {scan.task_id}
                     </a>
                   </strong>
+                </Td>
+                <Td dataLabel={columnNames.allFindings}>
+                  {scan.scan_results_url && (
+                    <strong>
+                      <a
+                        href={getHTMLScanUrl(scan.scan_results_url)}
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        results
+                      </a>
+                    </strong>
+                  )}
                 </Td>
               </Tr>
             ))}
